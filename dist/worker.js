8250a41e2ace0cac46e2ffc864e06163621f9879
dom111 <dom111@users.noreply.github.com>
2022-01-04 19:11:28 +0000

    Get command line flags working properly. Styling updates. Cleanup a few little bits - still lots to do there....

--- dist/worker.js
+++ dist/worker.js
@@ -1,11 +1,2 @@
-(()=>{importScripts("./lib/webperl.js");addEventListener("message",({data:a})=>{let{code:i,args:s,input:n}=a;Perl.output=(t,e)=>{if(e===2){postMessage({type:"output",error:t});return}postMessage({type:"output",output:t})},Perl.init(()=>{let t="",e="";Perl.start([]),Perl.stdin_buf+=n;let r=s.match(/F(\S+)?/);r?t=`@F = split/\\Q${r[1]??"".replace(/^\/|\/$/g,"")}/,$_;
-`+t:s.match(/a/)&&(t=`@F = grep$_,split/\\s+/,$_;
-`+t),s.match(/p/)&&(e+=`;
-print;
-`+e),s.match(/l/)&&(t=`$\\=$/;
-chomp;
-`+t),s.match(/[anpF]/)&&(t=`while(<STDIN>) {
-`+t,e+=`;
-}
-`);let l=Perl.eval([...[...t].map(p=>p.charCodeAt()),...i,...[...e].map(p=>p.charCodeAt())]);Perl.end(),postMessage({type:"done",result:l,output:Perl.stdout_buf})})});})();
+(()=>{importScripts("./lib/webperl.js");addEventListener("message",({data:s})=>{let{code:p,args:i,input:o}=s;Perl.output=(e,t)=>{if(t===2){postMessage({type:"output",error:e});return}postMessage({type:"output",output:e})},Perl.endAfterMain=!0,Perl.addStateChangeListener((e,t)=>{t==="Ended"&&postMessage({type:"done"})}),Perl.init(()=>{let e="",t="";Perl.stdin_buf+=o;let u=Perl.run([...[...e].map(r=>r.charCodeAt()),...p,...[...t].map(r=>r.charCodeAt())],i.split(/\s+/))})});})();
 //# sourceMappingURL=worker.js.map
--- js/worker.js
+++ js/worker.js
@@ -22,54 +22,36 @@ addEventListener('message', ({ data }) => {
     });
   };
 
+  Perl.endAfterMain = true;
+
+  Perl.addStateChangeListener((from, to) => {
+    if (to === 'Ended') {
+      postMessage({
+        type: 'done',
+      });
+    }
+  });
+
   Perl.init(() => {
     let prefix = '',
       suffix = '';
 
-    // TODO: look into why this isn't working for everything (-anpF).
-    // Perl.start(args.split(/\s+/));
-    Perl.start([]);
-
     Perl.stdin_buf += input;
 
-    // Force flushing output with `$|` - doesn't look like this is needed?
-    // const result = Perl.eval('$|++;\n' + code);
+    const result = Perl.run(
+      [
+        ...[...prefix].map((c) => c.charCodeAt()),
+        ...code,
+        ...[...suffix].map((c) => c.charCodeAt()),
+      ],
+      args.split(/\s+/)
+    );
+    // Perl.end();
 
-    const match = args.match(/F(\S+)?/);
-
-    if (match) {
-      prefix =
-        `@F = split/\\Q${match[1] ?? ''.replace(/^\/|\/$/g, '')}/,$_;\n` +
-        prefix;
-    } else if (args.match(/a/)) {
-      prefix = `@F = grep$_,split/\\s+/,$_;\n` + prefix;
-    }
-
-    if (args.match(/p/)) {
-      suffix += ';\nprint;\n' + suffix;
-    }
-
-    if (args.match(/l/)) {
-      prefix = '$\\=$/;\nchomp;\n' + prefix;
-    }
-
-    if (args.match(/[anpF]/)) {
-      prefix = 'while(<STDIN>) {\n' + prefix;
-      suffix += ';\n}\n';
-    }
-
-    const result = Perl.eval([
-      ...[...prefix].map((c) => c.charCodeAt()),
-      ...code,
-      ...[...suffix].map((c) => c.charCodeAt()),
-    ]);
-
-    Perl.end();
-
-    postMessage({
-      type: 'done',
-      result,
-      output: Perl.stdout_buf,
-    });
+    // postMessage({
+    //   type: 'done',
+    //   result,
+    //   output: Perl.stdout_buf,
+    // });
   });
 });
