d6a122f4ffd3cef1d234fdf0b1bde2c666bbeb8b
dom111 <dom111@users.noreply.github.com>
2022-01-20 20:04:55 +0000

    Add Hexdump decoder

--- js/app.ts
+++ js/app.ts
@@ -1,5 +1,6 @@
 import Base64 from './Decoders/Base64';
 import Default from './Decoders/Default';
+import Hexdump from './Decoders/Hexdump';
 import JavaScript from './langs/javascript-browser/JavaScript';
 import Perl from './langs/webperl-5.28.1/Perl';
 import UI from './UI';
@@ -7,7 +8,7 @@ import Xxd from './Decoders/Xxd';
 import { decoders } from './Decoders';
 import { langs } from './Langs';
 
-decoders.register(new Xxd(), new Base64(), new Default());
+decoders.register(new Xxd(), new Hexdump(), new Base64(), new Default());
 langs.register(new JavaScript(), new Perl());
 
 const ui = new UI();
